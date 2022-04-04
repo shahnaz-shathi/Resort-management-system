fa0272471317c4664358a2e7ede7b42a29baf909
shahnaz-shathi <78074879+shahnaz-shathi@users.noreply.github.com>
2022-04-04 13:47:11 +0600

    Add files via upload

---
+++ Database/Dininginfo.cs
@@ -0,0 +1,158 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Operations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Dininginfo : Form
+    {
+        public Dininginfo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            EDining eDining = new EDining();
+            ODining oDining = new ODining();
+            eDining.Id = txtId.Text;
+            eDining.Fooditem = txtFood.Text;
+            eDining.Price = txtPrice.Text;
+
+            int flag = oDining.Insert(eDining);
+            if (flag > 0)
+            {
+                MessageBox.Show("Inserted successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Inserted");
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            ODining oDining = new ODining();
+            SqlDataAdapter sqlDataAdapter = oDining.Show();
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            EDining eDining = new EDining();
+            ODining oDining= new ODining();
+            eDining.Id = txtId.Text;
+            eDining.Fooditem = txtFood.Text;
+            eDining.Price = txtPrice.Text;
+            int flag = oDining.Update(eDining);
+            if (flag > 0)
+            {
+                MessageBox.Show("Updated successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            EDining eDining = new EDining();
+            ODining oDining = new ODining();
+            eDining.Id = txtId.Text;
+            eDining.Fooditem = txtFood.Text;
+            eDining.Price = txtPrice.Text;
+
+            int flag = oDining.Delete(eDining);
+            if (flag > 0)
+            {
+                MessageBox.Show("Deleted successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+        }
+
+        private void homepageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Homepage().Show();
+            this.Hide();
+        }
+
+        private void customerInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Customer().Show();
+            this.Hide();
+        }
+
+        private void roomInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Roominfo().Show();
+            this.Hide();
+        }
+
+        private void employeeInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Employeeinfo().Show();
+            this.Hide();
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Login().Show();
+            this.Hide();
+        }
+
+        private void btnShow_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to view dining menu";
+        }
+
+        private void btnShow_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnInsert_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to insert dining menu";
+        }
+
+        private void btnInsert_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnUpdate_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to update dining menu";
+        }
+
+        private void btnUpdate_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnDelete_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to delete dining menu";
+        }
+
+        private void btnDelete_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+    }
+}
---
+++ Database/Employeeinfo.cs
@@ -0,0 +1,170 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Operations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database
+{
+    public partial class Employeeinfo : Form
+    {
+        public Employeeinfo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            EEmployee eEmployee = new EEmployee();
+            OEmployee oEmployee = new OEmployee();
+            eEmployee.Employeeid = txtId.Text;
+            eEmployee.Name = txtName.Text;
+            eEmployee.Email = txtEmail.Text;
+            eEmployee.Phone = txtPhone.Text;
+            eEmployee.Occupation = txtOccupation.Text;
+            eEmployee.Salary = txtSalary.Text;
+            int flag = oEmployee.Insert(eEmployee);
+            if (flag > 0)
+            {
+                MessageBox.Show("Inserted successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Inserted");
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            OEmployee oEmployee = new OEmployee();
+            SqlDataAdapter sqlDataAdapter = oEmployee.Show();
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            EEmployee eEmployee = new EEmployee();
+            OEmployee oEmployee = new OEmployee();
+            eEmployee.Employeeid = txtId.Text;
+            eEmployee.Name = txtName.Text;
+            eEmployee.Email = txtEmail.Text;
+            eEmployee.Phone = txtPhone.Text;
+            eEmployee.Occupation = txtOccupation.Text;
+            eEmployee.Salary = txtSalary.Text;
+            int flag = oEmployee.Update(eEmployee);
+            if (flag > 0)
+            {
+                MessageBox.Show("Updated successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            EEmployee eEmployee = new EEmployee();
+            OEmployee oEmployee = new OEmployee();
+            eEmployee.Employeeid = txtId.Text;
+            eEmployee.Name = txtName.Text;
+            eEmployee.Email = txtEmail.Text;
+            eEmployee.Phone = txtPhone.Text;
+            eEmployee.Occupation = txtOccupation.Text;
+            eEmployee.Salary = txtSalary.Text;
+            int flag = oEmployee.Delete(eEmployee);
+            if (flag > 0)
+            {
+                MessageBox.Show("Deleted successfully");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+        }
+
+        private void homepageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Homepage().Show();
+            this.Hide();
+        }
+
+        private void customerInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Customer().Show();
+            this.Hide();
+        }
+
+        private void roomInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Roominfo().Show();
+            this.Hide();
+        }
+
+        private void diningToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Dininginfo().Show();
+            this.Hide();
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Login().Show();
+            this.Hide();
+        }
+
+        private void btnShow_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to view employee data";
+        }
+
+        private void btnShow_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnInsert_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to insert employee data";
+        }
+
+        private void btnInsert_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnUpdate_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to update employee data";
+        }
+
+        private void btnUpdate_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+
+        private void btnDelete_MouseHover(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Click to delete employee data";
+        }
+
+        private void btnDelete_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+    }
+}
